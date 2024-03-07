c9a4df9bf70c6476e2c30c4ab120342d96833402
BergetAlex <alex.bergkrantz@gmail.com>
2024-03-07 11:47:04 +0100

    Added ouput pins

--- main.c
+++ main.c
@@ -4,8 +4,10 @@ byte tick = 1;
 
 void setup() {
     DDRB &= ~(1 << PB5); // Sätter pin 13 som input
-
     DDRB |= (1 << PB4); // Sätter pin 12 som output
+    DDRB |= (1 << PB3); // Sätter pin 11 som output
+    DDRB |= (1 << PB2); // Sätter pin 10 som output
+    DDRB |= (1 << PB1); // Sätter pin 9 som output
 }
 
 void loop() {
