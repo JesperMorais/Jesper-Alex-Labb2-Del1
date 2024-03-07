c107844782afe0bd32320da69f5371efd5c3d959
BergetAlex <alex.bergkrantz@gmail.com>
2024-03-07 11:54:12 +0100

    Added comments

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
-byte tick = 1;
-byte pin = 1;
+byte tick = 1; // Sätter tick till 1
+byte pin = 1; // Sätter pin till 1
 
 void setup() {
     DDRB &= ~(1 << PB5); // Sätter pin 13 som input
@@ -13,18 +13,18 @@ void setup() {
 
 void loop() {
     
-    PORTB |= (1 << pin); // Toggle pin 12 (HIGH)
+    PORTB |= (1 << pin); // Toggle pin x (HIGH)
     delay(100); //blink delay
-    PORTB &= ~(1 << pin); // Toggle pin 12 (LOW)
+    PORTB &= ~(1 << pin); // Toggle pin x (LOW)
     delay(tick*1000);
 
-    pin++;
-    tick++;
+    pin++; // Ökar pin med 1
+    tick++; // Ökar tick med 1
 
-    if(tick > 9)
-         tick = 1;
-    if(pin > 4)
-        pin = 1;
+    if(tick > 9) // Om tick är större än 9
+         tick = 1; // Sätt tick till 1
+    if(pin > 4) // Om pin är större än 4
+        pin = 1; // Sätt pin till 1
 
   
 }
