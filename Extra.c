22ab8c8b4093dec06ba31e105169a47f6a78f817
BergetAlex <alex.bergkrantz@gmail.com>
2024-03-20 10:46:33 +0100

    fixed volatile

--- Extra.c
+++ Extra.c
@@ -1,7 +1,13 @@
-#include <Arduino.h>
-
+#include <avr/io.h>
 #include <stdbool.h>
 #include <stdint.h>
+
+volatile uint8_t *ddrb = (uint8_t *)0x24;
+volatile uint8_t *portb = (uint8_t *)0x25;
+volatile uint8_t *pinb = (uint8_t *)0x23;
+volatile uint8_t *ddrd = (uint8_t *)0x2A;
+volatile uint8_t *pind = (uint8_t *)0x29;
+volatile uint8_t *portb = (uint8_t *)0x2B;
 
 //struct som innehåller all information om en led
 typedef struct Led {
@@ -15,26 +21,26 @@ typedef struct Led {
 } Led;
 
 void setup() {
-    DDRB &= ~(1 << PB5); 
-    DDRB |= (1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4);  // Sätter pin 1, 2, 3 och 4 som Digital output
+    *ddrb &= ~(1 << PB5); 
+    *ddrb |= (1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4);  // Sätter pin 1, 2, 3 och 4 som Digital output
 
-    DDRD &= ~(1 << PD7); // Sätter pin 7 som Digital input 
-    DDRD &= ~(1 << PD6); // Sätter pin 6 som Digital input
-    DDRD &= ~(1 << PD5); // Sätter pin 5 som Digital input
-    DDRD &= ~(1 << PD4); // Sätter pin 4 som Digital input  
+    *ddrd &= ~(1 << PD7); // Sätter pin 7 som Digital input 
+    *ddrd &= ~(1 << PD6); // Sätter pin 6 som Digital input
+    *ddrd &= ~(1 << PD5); // Sätter pin 5 som Digital input
+    *ddrd &= ~(1 << PD4); // Sätter pin 4 som Digital input  
 }
 
 //funktion som retunrerar 1 om någon av lamporna är tända
-int anyLedsOn(){
-    if(PINB & (1 << PB1) || PINB & (1 << PB2) || PINB & (1 << PB3) || PINB & (1 << PB4)){
+int anyLedsOn() {
+    if(*pinb & (1 << PB1) || *pinb & (1 << PB2) || *pinb & (1 << PB3) || *pinb & (1 << PB4)){
         return 1;
     }
     return 0;
 }
 
 //funktion som tänder lampan samt sätter uppdaterar relevant variabler
-void turnOnLamp(Led *led){
-    PORTB |= (1 << led->pin);
+void turnOnLamp(Led *led) {
+    *portb |= (1 << led->pin);
     led->isOn = true;
     led->blinkTimer = millis();
 }
@@ -44,7 +50,7 @@ void controllLed(Led *led) {
     if(led->isOn){
       
       if(millis() - led->blinkTimer >= led->blink){ //om tiden som gått sedan lampan började blinka är större än blinktiden        
-        PORTB &= ~(1 << led->pin); //stänger av leden
+        *portb &= ~(1 << led->pin); //stänger av leden
         led->isOn = false; //sätter isOn till false
         led->ledOfftimer = millis(); //sätter ledOfftimer till millis
       }
@@ -68,16 +74,16 @@ void loop() {
     static Led _PB3 = {3, 500,{6000, 2000}, 0, false,0,0};
     static Led _PB4 = {4, 500,{2000, 10000}, 0, false,0,0};
 
-    if(PIND & (1 << PD7) || PINB |= (1 << PB1)) { // Om knapp 7 är nedtryckt eller lampan är tänd
+    if(*pind & (1 << PD7) || *pinb |= (1 << PB1)) { // Om knapp 7 är nedtryckt eller lampan är tänd
         controllLed(&_PB1); 
     }
-    if(PIND & (1 << PD6) || PINB |= (1 << PB2)) { // om knapp 6 är nedtryckt eller lampan är tänd
+    if(*pind & (1 << PD6) || *pinb |= (1 << PB2)) { // om knapp 6 är nedtryckt eller lampan är tänd
         controllLed(&_PB2);
     }
-    if(PIND & (1 << PD5) || PINB |= (1 << PB3)) { // Om knapp 5 är nedtryckt eller lampan är tänd 
+    if(*pind & (1 << PD5) || *pinb |= (1 << PB3)) { // Om knapp 5 är nedtryckt eller lampan är tänd 
         controllLed(&_PB3);
     }
-    if(PIND & (1 << PD4) || PINB |= (1 << PB4)) { // Om knapp 4 är nedtryckt eller lampan är tänd
+    if(*pind & (1 << PD4) || *pinb |= (1 << PB4)) { // Om knapp 4 är nedtryckt eller lampan är tänd
         controllLed(&_PB4);
     }
 
